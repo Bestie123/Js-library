d49a176a066bf974f932e3a027f45db3bc1e1b80
Mikhail <misha1997-09@mail.ru>
2022-11-19 01:39:04 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,27 +1,28 @@
   (async () => {
 	  
 	        ObjectFollowedList={} // записываем все объекты в объект в оперативную память для воможно более быстрого доступа при переборе комнат
-//FollowedList.forEach(function(item){ObjectFollowedList[item.id]=item.FollowedCategory})
-	  
-   // создание списка подключений
-// удаление подключения при закрытии страницы
-// запрос онлайн комнат и возвращение списка
-
-  self.PageList={};
+	   self.PageList={};
 var connections = 0;
 var FollowedList={};
 var FollowedCategoryList={};
- self.addEventListener("connect", function (e) { //отслеживание подключения новых вкладок
-     var port = e.ports[0];
-     port.number= connections;
-PageList[connections]=port
-	connections++;
-
-	port.addEventListener("message", function (e) {
-        console.log(e);
-        if(e.data[0]=='ClosePage'){             //отслеживание события закрытия вкладок и удаление подключений
-         delete PageList[e.currentTarget.number]
-        }else if(e.data[0]=='RoomList'){    // событие ответа с онлайн комнатами
+//FollowedList.forEach(function(item){ObjectFollowedList[item.id]=item.FollowedCategory})
+	  
+	      function Roomif(e) {
+		      switch(e.data[1]){
+			      case 'RetGetAllFollowedRoomListInDB':{
+console.log(e.data[2]) // ответ с подписками
+}
+			      case 'RetSetFollowdRoomListInDB':{
+console.log(e.data[2]) // ответ true если подписка успешно сохранена	
+}
+			      case 'RetDeleteFollowdRoomListInDB':{
+console.log(e.data[2]) // ответ true если подписка успешно удалена	
+}
+	      }
+	      }
+	  
+	  function GetOnlineRoomList(e){ // Обработка полученного списка онлайн комнат
+	  	// событие ответа с онлайн комнатами
             console.log(e.data[1])
 FollowedCategoryList={};
             e.data[1].forEach(function(item){
@@ -34,16 +35,32 @@ FollowedCategoryList[FollowedList[item[0]]]=[];
   FollowedCategoryList[ObjectFollowedList[item[0]]].push(item)  //вносим в категорию комнату и ее html код
   // + сортировка по категориям
     //сортировка комнат в блоки с категориями
-}else if(e.data[0]=='RetGetAllFollowedRoomListInDB'){
-console.log(e.data[1]) // ответ с подписками
-}else if(e.data[0]=='RetSetFollowdRoomListInDB'){
-console.log(e.data[1]) // ответ true если подписка успешно сохранена	
-}else if(e.data[0]=='RetDeleteFollowdRoomListInDB'){
-console.log(e.data[1]) // ответ true если подписка успешно удалена	
-}
+
+	  }
+   // создание списка подключений
+// удаление подключения при закрытии страницы
+// запрос онлайн комнат и возвращение списка
+ self.addEventListener("connect", function (e) { //отслеживание подключения новых вкладок
+     var port = e.ports[0];
+     port.number= connections;
+PageList[connections]=port
+	connections++;
+
+	port.addEventListener("message", function (e) {
+	switch (e.data[0]){
+		case 'ClosePage':{             //отслеживание события закрытия вкладок и удаление подключений
+         delete PageList[e.currentTarget.number]
+        }
+		case'RoomList':{ 
+		GetOnlineRoomList(e);
+	}
+		case'DB':{
+Roomif(e);	 
+	 }
 		    
 })
         }
+		       }
 	}, false);
 
 	port.start();
